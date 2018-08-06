f095b1a16988ecf146163b03230af72efe9fa432
Philipp Heuer <git@philippheuer.me>
2018-08-06 20:21:52 +0200

    chore: export ld library path

--- docker/rootfs/usr/local/bin/ghostpp.sh
+++ docker/rootfs/usr/local/bin/ghostpp.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# LD Library Path
+export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
 # create environment based configuration
 envsubst < /opt/ghostpp/docker.cfg > /opt/ghostpp/ghost.cfg
 
